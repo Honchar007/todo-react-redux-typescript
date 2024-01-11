869e54d59c5b16ac191619200763220f8453ee13
Honchar007 <andrey2002335@gmail.com>
2024-01-11 02:20:18 +0200

    add functional bar component

--- src/app/components/FunctionalBar/FunctionalBar.tsx
+++ src/app/components/FunctionalBar/FunctionalBar.tsx
@@ -3,9 +3,11 @@ import StyledButton from '../StyledButton/StyledButton';
 
 // styles
 import './FunctionalBar.scss';
+import TodoItem from '../TodoItem/TodoItem';
 
 function FunctionalBar () {
   const [todo, setTodo] = useState('');
+  const [desc, setDesc] = useState('');
 
   const add = () => {}
 
@@ -20,6 +22,14 @@ function FunctionalBar () {
         placeholder='I wanna buy...'
         className='input-todo'
       />
+      <input
+        type='text'
+        name='description'
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+        placeholder='Something...'
+        className='input-todo'
+      />
       <div className='btn-wrapper'>
         <StyledButton
           onClick={add}
@@ -29,7 +39,18 @@ function FunctionalBar () {
           onClick={add}
           name='Edit'
         />
+        <StyledButton
+          onClick={add}
+          name='Cancel'
+        />
       </div>
+      <TodoItem
+        id={0}
+        checked={true}
+        name='Go to ATB and buy bread'
+        description='Only cutted bread'
+        date={new Date()}
+      />
     </div>
    );
 }
